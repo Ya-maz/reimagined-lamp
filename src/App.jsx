139fce698ba9fe2550a6d37f961c6a86cb864634
Ya-maz <vochko50@gmail.com>
2022-04-25 16:03:00 +0300

    I18next

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import "./App.scss";
 import {Button} from "./components/Button";
+import {Button as StrapButton} from "reactstrap";
+import {FormBox} from "./FormBox";
 
 function App() {
   return (
     <div className="App">
-      <Button onClick={null} title="click" type="success" />
-      <Button onClick={null} title="click" size="md" type="error" />
-      <Button onClick={null} title="click" size="lg" type="info" />
-      <Button
-        onClick={null}
-        disabled={true}
-        title="click"
-        size="lg"
-        type="info"
-      />
+      <div>
+        <Button onClick={null} title="click" type="success" />
+        <Button onClick={null} title="click" sizeau="md" type="error" />
+        <Button onClick={null} title="click" size="lg" type="info" />
+        <Button
+          onClick={null}
+          disabled={true}
+          title="click"
+          size="lg"
+          type="info"
+        />
+      </div>
+      <div>
+        <StrapButton size="sm" color="warning">
+          RU
+        </StrapButton>
+        <StrapButton size="lg" color="warning">
+          EN
+        </StrapButton>
+      </div>
+      <FormBox />
     </div>
   );
 }
---
+++ src/FormBox.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Form from "reactstrap";
+
+export const FormBox = () => {
+  return (
+    <Form>
+      <h1>I'm form</h1>
+    </Form>
+  );
+};
